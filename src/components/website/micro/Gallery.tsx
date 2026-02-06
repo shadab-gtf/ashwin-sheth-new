46354786c85c38c691374f7319e3f80cc72d87dc
Kuldeep Chaudhary <kuldeep.chaudhary@gtftechnologies.com>
2026-02-06 12:06:34 +0530

    microsite updated

--- src/components/website/micro/Gallery.tsx
+++ src/components/website/micro/Gallery.tsx
@@ -4,7 +4,6 @@ import CommonSlider from "./CommonSlider"
 import Image from "next/image"
 import Lightbox from "yet-another-react-lightbox"
 import useGsapReveal from '@/hooks/useGsapReveal'
-import CardsSlider from "@/components/common/slider/CardsSlider"
 
 const data = [
   { src: "/assets/images/micro/gallery/1.jpg", alt: "image 1" },
@@ -23,19 +22,24 @@ const Gallery = () => {
   return (
     <>
       <section className="bg-[#FEF7F0] pb-[50px] md:pb-[100px]">
-        <h2 data-direction="bottom" className="reveal-text  text-[32px] leading-[50px] font-medium  tracking-[1px] capitalize text-[#F0801B] text-center">
-          photos gallery
+        <h2 data-direction="bottom" className="reveal-text w-full max-w-[800px] mx-auto  text-[32px] leading-[50px] font-medium  tracking-[1px] capitalize text-[#F0801B] text-center">
+          Every Image Tells a Story of Elegance, Captured in Time.
         </h2>
 
-        <div className="mt-[50px] md:w-[60%] mx-auto">
-          <CardsSlider
+        <div className="mt-[50px]">
+          <CommonSlider
             data={data}
-            loop={true}
-              containerClassName="w-full"
-
-            renderCard={(item, i) => (
+            loop={false}
+            showProgress={true}
+            slidesPerView={1.2}
+            breakpoints={{
+              0: { slidesPerView: 1 },
+              768: { slidesPerView: 1.2 },
+            }}
+            spaceBetween={20}
+            renderItem={(item, i) => (
               <div
-                className="relative cursor-pointer group h-[450px] 2xl:h-[500px]"
+                className="relative cursor-pointer group h-[550px]"
                 onClick={() => {
                   setIndex(i)
                   setOpen(true)
